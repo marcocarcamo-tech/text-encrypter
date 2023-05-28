206bc100d94f830f7648bdfde49e44d303069ee6
Marco Carcamo <macdevg@gmail.com>
2023-05-28 02:15:54 -0600

    Ajust screen height for devices

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import './styles/App.scss'
 function App() {
   return (
     <div className="App">
-      
-      <div className='programs'>
-      <a href="https://github.com/marcocarcamo-tech" target='_blank'><img src={github} alt="Github-icon" /><p className='icon'>Github</p></a>
-      <a href="https://www.linkedin.com/in/marcocarcamo/" target='_blank'><img src={linkedin} alt="LinkedIn-icon" /><p className='icon'>LinkedIn</p></a>
-      </div>
-      
-      <Window>
+      <section className='main'>
+        <div className='programs'>
+        <a href="https://github.com/marcocarcamo-tech" target='_blank'><img src={github} alt="Github-icon" /><p className='icon'>Github</p></a>
+        <a href="https://www.linkedin.com/in/marcocarcamo/" target='_blank'><img src={linkedin} alt="LinkedIn-icon" /><p className='icon'>LinkedIn</p></a>
+        </div>
         
-      </Window>
+        <Window></Window>
+      </section>
+      
       <footer>
         <p style={{textAlign: 'center'}}>Developed with <img src="https://img.icons8.com/fluency/48/null/pixel-heart.png" alt='pixelart-heart' style={{width: '1rem'}} /> by Marco Carcamo</p>
       </footer>
